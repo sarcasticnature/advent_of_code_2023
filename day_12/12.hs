ee4311d086976083c655b5af18f1f9e77be7ce94
Jake Keller <jakeller3630@gmail.com>
2023-12-14 19:47:56 -0600

    Fix day 12 p1

--- day_12/12.hs
+++ day_12/12.hs
@@ -6,9 +6,7 @@ main = do
     let filename = head filename_list
     contents <- readFile filename
     putStrLn "Part 1:"
-    print $ map combos $ parse $ lines contents
-    putStrLn "\n"
-    print $ combos' $ last $ parse $ lines contents
+    print $ sum $ map combos $ parse $ lines contents
     --putStrLn "Part 2:"
 
 parse :: [String] -> [(String, [Int])]
@@ -20,7 +18,7 @@ combos (ss,[]) = if '#' `elem` ss then 0 else 1
 combos ("",_) = 0
 combos (ss,n:ns) =
     let springFit ss' = length ss' == n && all (\c -> c == '#' || c == '?') ss'
-        dropped ss' = if d == [] then [] else tail d
+        dropped ss' = if null d then [] else tail d
             where d = dropWhile (== '#') ss'
         peek = combos (dropped $ drop n ss, ns)
         hasSpace = case drop n ss of []    -> True
@@ -30,28 +28,6 @@ combos (ss,n:ns) =
         valid = if springFit (take n ss) && hasSpace
                 then peek
                 else 0
-        munch = case head ss of '#' -> combos (dropped ss, n:ns)
+        munch = case head ss of '#' -> 0
                                 _   -> combos (tail ss, n:ns)
     in  munch + valid
-
--- debug
-
-combos' :: (String, [Int]) -> [(String, [Int])]
-combos' (ss,[]) = if '#' `elem` ss then [] else [(ss, [])]
-combos' ("",_) = []
-combos' (ss,n:ns) =
-    let springFit ss' = length ss' == n && all (\c -> c == '#' || c == '?') ss'
-        dropped = if d == [] then [] else tail d
-            where d = dropWhile (== '#') ss
-        peek = combos' (drop (n + 1) ss, ns)
-        hasSpace = case drop n ss of []    -> True
-                                     '.':_ -> True
-                                     '?':_ -> True
-                                     _     -> False
-        valid = if springFit (take n ss) && peek /= [] && hasSpace
-                then [(ss, n:ns)]
-                else []
-        munch = case head ss of '#' -> combos' (dropped, n:ns)
-                                _   -> combos' (tail ss, n:ns)
-    in  munch ++ valid
-
